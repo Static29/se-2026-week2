e7f9c291ae1c2299335d752e5603c5195bf97d81
sw209 <jsw209@naver.com>
2026-09-11 19:21:47 +0900

    hadChild, height 메서드 구현
    Implement hasChild and height

--- Node.java
+++ Node.java
@@ -14,4 +14,26 @@ public class Node {
     public void addChild(Node child) {
         this.children.add(child);
     }
+
+    // 현재 노드에 자식노드가 있는지 확인하는 메서드
+    public boolean hasChild() {
+        return !this.children.isEmpty();
+    }
+    
+    // 재귀를 통해 자식노드부터 루트까지 높이를 계산하는 메서드
+    public int height() {
+        if (!hasChild()) {
+            return 1;
+        } else {
+            int maxHeight = 0;
+            for (Node child : children) {
+                int childHeight = child.height();
+                if (childHeight > maxHeight) {
+                    maxHeight = childHeight;
+                }
+            }
+            return maxHeight + 1;
+        }
+    }
+
 }
